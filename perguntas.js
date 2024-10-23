78c345f2b36e8d1280e24265793aaf8e591bb547
jackpereton <00001112065313sp@al.educacao.sp.gov.br>
2024-10-23 10:21:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'historia',
+    'o que aconteceu em 2001 que impactou o mundo?',
+    'O dia 11 de setembro de 2001 é marcado aos atentados terroristas que ocorreram nos Estados Unidos nas torres gemeas'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capital de estados unidos?',
+    'Washington D.C. é a capital e corresponde à área do Distrito Federal dos Estados Unidos'
+)
+
+criaCartao(
+    'cinema',
+    'qual é o filme que deu mais bilheteria do mundo?',
+    'maior bilheteria do cinema mundial é 1º Avatar 2009'
+)
+
+criaCartao(
+    'cinema',
+    'qual melhor filme em stop motion?',
+    'Coraline e o Mundo Secreto'
+)
